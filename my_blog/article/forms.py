6bdb95c38468510fea8595d1f3d0bb5e2307ff9b
WoodYu <1041750660@qq.com>
2020-08-15 00:27:51 +0800

    myblog

---
+++ my_blog/article/forms.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from django import forms
+from .models import ArticlePost
+
+
+class ArticlePostForm(forms.ModelForm):
+    class Meta:
+        model = ArticlePost
+        fields = ('title','body','tags','avatar')
---
+++ my_blog/my_blog/urls.py
@@ -0,0 +1,43 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import notifications.urls
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from article.views import article_list
+
+# 存放了映射关系的列表
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # home
+    path('', article_list, name='home'),
+    # 重置密码app
+    path('password-reset/', include('password_reset.urls')),
+    # 新增代码，配置app的url
+    path('article/', include('article.urls', namespace='article')),
+    # 用户管理
+    path('userprofile/', include('userprofile.urls', namespace='userprofile')),
+    # 评论
+    path('comment/', include('comment.urls', namespace='comment')),
+    # djang-notifications
+    path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
+    # notice
+    path('notice/', include('notice.urls', namespace='notice')),
+    # django-allauth
+    path('accounts/', include('allauth.urls')),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

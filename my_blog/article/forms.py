c25eb077be1e18979fe3054bf46222cddf27c14f
WoodYu <1041750660@qq.com>
2020-08-16 16:43:58 +0800

    fuck

--- my_blog/article/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from django import forms
-from .models import ArticlePost
-
-
-class ArticlePostForm(forms.ModelForm):
-    class Meta:
-        model = ArticlePost
-        fields = ('title','body','tags','avatar')
